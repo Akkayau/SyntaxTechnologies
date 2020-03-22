9d2d7656fc3ab66c05da898836d7877289b93053
SyntaxTechnologies <instructor@syntaxtechs.com>
2020-03-22 14:48:26 -0400

    Methods with return type more practice

--- src/com/syntax/class15/MethodsReturningValue.java
+++ src/com/syntax/class15/MethodsReturningValue.java
@@ -28,10 +28,10 @@ public class MethodsReturningValue {
 		System.out.println("Largest number " + num + " is odd number:" + flag);
 
 	}
-
 	// create a method that returns largest number from 2 given numbers
 	int largest(int a, int b) {
 		int largest;
+		
 		if (a > b) {
 			largest = a;
 		} else {
