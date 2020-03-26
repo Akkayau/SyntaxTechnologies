c1cb0123e7054de15b1195301534e69a9fd8e020
SyntaxTechnologies <instructor@syntaxtechs.com>
2020-03-25 21:23:29 -0400

    Access Modifiers

--- src/com/syntax/class17/StaticKeywordTest.java
+++ src/com/syntax/class17/StaticKeywordTest.java
@@ -3,7 +3,6 @@ package com.syntax.class17;
 public class StaticKeywordTest {
 
 	public static void main(String[] args) {
-		
 		//access static members of another class
 		StaticKeyword.brand="Android";
 		StaticKeyword.touchScreen=true;
