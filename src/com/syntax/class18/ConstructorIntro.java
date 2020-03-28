4358d8feb36061aff58664bc9f13c495cb52f54b
SyntaxTechnologies <instructor@syntaxtechs.com>
2020-03-28 12:23:41 -0400

    Types of parameters

--- src/com/syntax/class18/ConstructorIntro.java
+++ src/com/syntax/class18/ConstructorIntro.java
@@ -14,13 +14,12 @@ public class ConstructorIntro {
 	}
 	
 	public static void main(String[] args) {
-		
+							// = creating an Object and calling a constructor
 		ConstructorIntro obj=new ConstructorIntro();
 		
 		System.out.println("Code after constructor");
-		
+		//calling method
 		obj.hello();
-
 	}
 	
 	void hello() {
