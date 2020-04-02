c4447656797289bea123660f4ffc8b64cf1655ec
Syntaxtechnologies <instructor@syntaxtechs.com>
2020-04-01 20:23:14 -0400

    Super call to parent constructor

--- src/com/syntax/class20/Doctor.java
+++ src/com/syntax/class20/Doctor.java
@@ -1,20 +1,17 @@
 package com.syntax.class20;
 
 public class Doctor {
-
 	int doctorId;
 	String name;
-
-	public Doctor(String name, int doctorId) {
-		this.name = name;
-		this.doctorId = doctorId;
-	}
 }
 
 class Dentist extends Doctor {
-	
-	Dentist(String name, int doctorId) {
-		// compiler will try to make call to parent class constructor super();
-		super(name, doctorId);
+
+	String cert;
+
+	Dentist(String name, int doctorId, String cert) {
+		super.name = name;
+		super.doctorId = doctorId;
+		this.cert = cert;
 	}
 }
