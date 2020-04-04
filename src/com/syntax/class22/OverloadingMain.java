3532cb3199757726f499b0942f7edefab9b95784
Syntaxtechnologies <instructor@syntaxtechs.com>
2020-04-04 12:57:12 -0400

    Method overriding

--- src/com/syntax/class22/OverloadingMain.java
+++ src/com/syntax/class22/OverloadingMain.java
@@ -2,12 +2,14 @@ package com.syntax.class22;
 
 public class OverloadingMain {
 	
+	static String str="Syntax";
+	
 	public static void main(int[] args) {
 		System.out.println("Method with int array arguments");
 	}
 	
 	public static void main(String args) {
-		System.out.println("Method with String argument");
+		System.out.println("Method with String argument "+args);
 	}
 	
 	public static void main(String args, String args1) {
@@ -25,10 +27,10 @@ public class OverloadingMain {
 	public static void main(String[] args) {//Java always looks for this main method with this signature
 		System.out.println("Method with String array arguments");
 		OverloadingMain.main("str");
-		main("str");
+		main(str);
 		main("hello", "hi");
 		//int[] arr=new int[2];
 		main(new int[2]);
 	}
-	
 }
+//BREAK 11:40-11:55
