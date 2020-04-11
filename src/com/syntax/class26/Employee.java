995d46b30b344a44d7582e659cb06c7c152ffe42
SyntaxTechnologies <instructor@syntaxtechs.com>
2020-04-11 12:42:01 -0400

    Encapsulation Example

--- src/com/syntax/class26/Employee.java
+++ src/com/syntax/class26/Employee.java
@@ -8,31 +8,32 @@ public class Employee {
 	private double salary;
 	
 	//define public methods to give an access to private variables
-	//getters
-	public String getName() {
-		return name;
-	}
 	
-	public int getAge() {
-		return age;
-	}
-	
-	public double getSalary() {
-		return salary;
-	}
-	
-	//setters
 	public void setName(String name) {
 		if(!name.isEmpty() && name.length()>3) {
 			this.name=name;
 		}
 	}
 	
+	public String getName() {
+		return name;
+	}
+	
 	public void setAge(int age) {
-		this.age=age;
+		if(age>1) {
+			this.age=age;
+		}
+	}
+	
+	public int getAge() {
+		return age;
 	}
 	
 	public void setSalary(double salary) {
 		this.salary=salary;
-	}
+	} 
+	
+	public double getSalary() {
+		return salary;
+	}	
 }
--- src/com/syntax/class26/EncapsulationTest.java
+++ src/com/syntax/class26/EncapsulationTest.java
@@ -4,11 +4,9 @@ public class EncapsulationTest {
 
 	public static void main(String[] args) {
 		
-		
 		Employee emp=new Employee();
 		emp.setName("");
 		System.out.println(emp.getName());
 		
-		//break till 12:10
 	}
 }
